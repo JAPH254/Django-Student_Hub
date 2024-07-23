c0a59034dbb52627525e989cbccbe512900378cf
JAPH254 <omanyujapheth@gmail.com>
2024-07-23 15:54:21 +0300

    fix: module conflict resolved

--- membersAuth/models.py
+++ membersAuth/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     profile_picture = models.ImageField(upload_to='images/', null=True, blank=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.username
